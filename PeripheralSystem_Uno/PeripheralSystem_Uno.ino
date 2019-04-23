90e12d5c437ca93d0370cec071ebde6de576b5be
Eun T. Ki <euntae@uw.edu>
2019-04-23 16:58:02 -0700

    Testing Commit

--- PeripheralSystem_Uno/PeripheralSystem_Uno.ino
+++ PeripheralSystem_Uno/PeripheralSystem_Uno.ino
@@ -16,33 +16,117 @@ Implement:   All the modules that should be in UNO
 
 (*&()*#&$()*#&$(*&()*#&$()#*&$^#^#*&^*N C(*#&(* # &#)(&#)%(&#{)(*&#(*$&#(* #HIUWHDKJHCI@U$Y@(*@ V) @)*&$@)(&$)@*$&@(*$&@)($&@($&@()*$@)($^@)&($*/
 
+int tempCount;
+int systCount;
+int diastCount;
+int pulseCount;
+bool systolicFlag;
 
 void setup()
 {
   // running on the uno - connect to tx1 and rx1 on the mega and to rx and tx on the uno
   // start serial port at 9600 bps and wait for serial port on the uno to open:
   Serial.begin(9600);
-
+  tempCount = 0;
+  systCount = 0;
+  diastCount = 0;
+  pulseCount = 0;
+  systolicFlag = false;
 }
 
 
 void loop()
 {
-   // EXAMPLE ON HOW TO READ FROM MEGA
-  
   //  read incoming byte from the mega
   while(Serial.available()<2)
   {
     // just wait
-    }
+  }
   byte inbyte1 = Serial.read();
   byte inbyte2 = Serial.read();
-
+  switch(){
+    
+  }
   //  write the data back
   Serial.write(inbyte2+inbyte1);
-  Serial.write(inbyte2-2);
-  Serial.write((byte)Serial.available());
+}
 
+void temperature(struct* md)
+{
+  bool flag = true;
+  if(md.temperatureRaw < 50 && flag) {
+    if(md.temperatureRaw > 50) {
+      flag = false;
+    }
+    if(tempCount % 2 == 0) {
+      md.temperatureRaw += 2;
+    } else {
+      md.temperatureRaw--;
+    }
+  } else if (md.temperatureRaw > 15 && !flag){
+    if(md.temperatureRaw < 15) {
+      flag = true;
+    }
+    if(tempCount % 2 == 0) {
+      md.temperatureRaw--;
+    } else {
+      md.temperatureRaw += 2;
+    }
+  }
+}
+
+void systolicPress(struct* md) {
+  if(md.systolicPressRaw < 100) {
+    systolicFlag = false;
+    if(systCount % 2 == 0) {
+      md.systolicPressRaw += 3;
+    } else {
+      md.systolicPressRaw--;
+    }
+  } else {
+    systolicFlag = true;
+  }
+}
+
+void diastolicPress(struct* md) {
+  if(md.diastsolicPressRaw < 100) {
+    diastolicFlag = false;
+    if(diastCount % 2 == 0) {
+      md.diastolicPressRaw += 3;
+    } else {
+      md.diastolicPressRaw--;
+    }
+  } else {
+    diastolicFlag = true;
+  }
+}
+
+void pulseRate(struct* md)
+{
+  bool flag = true;
+  if(md.pulseRateRaw <= 40 && flag) {
+    if(md.temperatureRaw > 40) {
+      flag = false;
+    }
+    if(tempCount % 2 == 0) {
+      md.temperatureRaw--;
+    } else {
+      md.temperatureRaw += 3;
+    }
+  } else if (md.temperatureRaw > 15 && !flag){
+    if(md.temperatureRaw < 15) {
+      flag = true;
+    }
+    if(tempCount % 2 == 0) {
+      md.temperatureRaw += 3;
+    } else {
+      md.temperatureRaw--;
+    }
+  }
+}
+
+void compute(struct* md) {
+  
 }
 
 //  end EE 474 code
