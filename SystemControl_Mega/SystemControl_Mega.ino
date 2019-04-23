bfe1718dc2c880179b4457f28e94322f6991f88d
Matt XU <xzy1998@uw.edu>
2019-04-23 16:56:55 -0700

    some definition

--- SystemControl_Mega/SystemControl_Mega.ino
+++ SystemControl_Mega/SystemControl_Mega.ino
@@ -1,9 +1,3 @@
-//  container for read data
- char inbyte;
-
-//  EE 474 code on the uno
-
-
 /* (!!!!!!!   DELETE THIS COMMENT BEFORE TURNING IN !!!!!!!!!!!!!!! )F(G^OPJ)(UB:H)(B"_&V(*^T{(^%*^%(*&^_(*&*^<(*^(*^&^*_)>&)H:^T &(^(*&^(*P)
  *  In this file, 
 
@@ -17,19 +11,141 @@ Implement:   All the modules that should be in MEGA (Tasks, data, scheduler)
 
 
 
+
+
+//MeasureData Struct
+typedef struct
+{
+  unsigned int* tempRawPtr;
+  unsigned int* systoRawPtr;
+  unsigned int* diastoRawPtr;
+  unsigned int* pulseRawPtr;
+} MeasureData; 
+ 
+ //ComputeData Struct
+ 
+typedef struct
+{
+  unsigned int* tempRawPtr;
+  unsigned int* systoRawPtr;
+  unsigned int* diastoRawPtr;
+  unsigned int* pulseRawPtr;
+  unsigned char** tempCorrected;
+  unsigned char** sysPressCorrected;
+  unsigned char** diasCorrected;
+  unsigned char** prCorrected;
+  
+} ComputeData; 
+
+ //DisplayData
+typedef struct
+{
+  unsigned char** tempCorrected;
+  unsigned char** sysPressCorrected;
+  unsigned char** diasCorrected;
+  unsigned char** prCorrected;
+  unsigned short* batteryState;
+} DisplayData;
+
+ // WarningAlarmData
+typedef struct
+{
+  unsigned int* tempRawPtr;
+  unsigned int* systoRawPtr;
+  unsigned int* diastoRawPtr;
+  unsigned int* pulseRawPtr;
+  unsigned short* batteryState;
+   
+} WarningAlarmData;
+
+// StatusData
+typedef struct
+{
+  unsigned short* batteryState;
+   
+} StatusData;
+
+ // SchedulerData
+typedef struct
+{
+  int none;
+   
+} SchedulerData;
+
+typedef struct
+{
+   void (*myTask)(void*);
+   void* taskDataPtr;
+   
+} TCB;
+ 
+enum _myBool { FALSE = 0, TRUE = 1 };
+typedef enum _myBool Bool;
+
+
+
+// function headers
+// major functions
+void Initialization();
+void Schedule(TCB* tasks[], int taskCount);
+// task functions
+void Measure(void* data);
+void Compute(void* data);
+void Display(void* data);
+void Status(void* data);
+void AlarmsAndWarning(void* data);
+// Intrasystem Communication functions
+
+
+
+
+
+// Global Variables for Measurements
+unsigned int temperatureRaw = 75;
+unsigned int systolicPressRaw = 80;
+unsigned int diastolicPressRaw = 80;
+unsigned int pulseRateRaw = 50;
+// Global Variables for Display
+unsigned char* tempCorrected = NULL;
+unsigned char* systolicPressCorrected = NULL;
+unsigned char* diastolicPressCorrected = NULL;
+unsigned char* pulseRateCorrected = NULL;
+// Global Variables for Status
+unsigned short batteryState = 200;
+// Global Variables for Alarm
+unsigned char bpOutOfRange = 0;
+unsigned char tempOutoRange = 0;
+unsigned char pulseOutOfRange = 0;
+//Global Variables for Warning
+Bool bpHigh = FALSE;
+Bool tempHigh = FALSE;
+Bool pulseLow = FALSE;
+
+
+/******************************************
+* function name: setup
+* function inputs: None
+* function outputs: None
+* function description: This is the function
+*                       for arduino to initializa
+*                       the processor
+* author: Matt & Sabrina
+******************************************/ 
 void setup()
 {
-  // running on the uno - connect to tx1 and rx1 on the mega and to rx and tx on the uno
-  // start serial port at 9600 bps and wait for serial port on the uno to open:
-
-  // initialize both serials. Serial1 for writing and reading from UNO
-  // Serial is for printing in the monitor console
-  // Add the TFT screen thing later
+  // initialize the communication between Mega and Uno
   Serial1.begin(9600);
   Serial.begin(9600);
 }
 
-
+/******************************************
+* function name: loop
+* function inputs: None
+* function outputs: None
+* function description: This is the function
+*                       our Mega will run forever
+* author: Matt & Sabrina
+******************************************/ 
 void loop()
 {
   // EXAMPLE FOR SENDING MESSAGE TO UNO
@@ -52,5 +168,19 @@ void loop()
   Serial.println(recv2);
   delay(1000);
 }
+
+/******************************************
+* function name: loop
+* function inputs: None
+* function outputs: None
+* function description: This is the function
+*                       our Mega will run forever
+* author: Matt & Sabrina
+******************************************/ 
+void loop(){
+  
+}
+
+
 
 //  end EE 474 code
